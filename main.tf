b6a93f8ec0cfbecdb48693ff149f3bb94b165e09
Galya777 <galdodova@abv.bg>
2024-12-31 15:04:08 +0200

    more deployment files

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws" # Specify the source of the AWS provider
+      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # Set the AWS region to US East (N. Virginia)
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  key_name      = "mykeypair"
+
+  tags = {
+    Name = "web-server"
+  }
+}
