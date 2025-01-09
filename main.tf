7cbafb76e6b579b20b02ef3c575640481d7ee8d6
Galya777 <galdodova@abv.bg>
2025-01-09 14:26:30 +0200

    changes in terraform

--- main.tf
+++ main.tf
@@ -21,4 +21,37 @@ resource "aws_instance" "web" {
   tags = {
     Name = "web-server"
   }
+  network_interface {
+    device_index          = 0
+    network_interface_id  = aws_network_interface.example.id
+  }
+}
+
+resource "aws_network_interface" "example" {
+  subnet_id   = aws_subnet.example.id
+  security_groups = [aws_security_group.example.id]
+}
+
+resource "aws_eip" "example" {
+  instance = aws_instance.web.id
+}
+
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "example" {
+  vpc_id = aws_vpc.example.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
